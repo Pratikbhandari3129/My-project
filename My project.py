6da413c3e3af510148c6a44b929ac96a202f4ae0
Pratikbhandari3129 <120951556+Pratikbhandari3129@users.noreply.github.com>
2023-01-03 21:24:26 +0530

    Add files via upload

---
+++ My project.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# "My Jupyter Notebook on IBM Watson Studio",
+
+# My and is **Pratik Bhandari**.
+# My current occupation is quality analyst
+
+# *I am interested in data science because i love to work with data so i want to lern something new and also i have seen videos about data scientist how they work with data so that's way i want to be a data scientist*.
+
+# In next line i will create line plot
+
+# In[5]:
+
+
+a = (1,2,3,4)
+b = (-1,+2,-3,+4)
+
+
+# In[6]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[7]:
+
+
+plt.plot(a,b)
+plt.show()
+
+
+# * Data Scientist
+# * Data Analyst
+# * Data Mining
+
+# Horizontal
+# ***
+
+# 1. Data scientist
+# 2. Data Analyst
+# 3. Deep lerning
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
